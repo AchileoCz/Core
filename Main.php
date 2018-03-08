4e6e5c618c38b08a6433dc8eab3c64093ca2db91
NapolGamer TH <knightnapol15@gmail.com>
2018-03-08 17:17:15 +0700

    Create new file

---
+++ Main.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace core;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use pocketmine\Player;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat as TF;
+
+class Main extends PluginBase{
+   
+   public function onEnable()
+   {
+   }
+}
