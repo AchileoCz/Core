63204045601b451660daca8e8a60b183d5361f15
NapolGamer TH <knightnapol15@gmail.com>
2018-03-08 17:41:23 +0700

    Update Main.php

--- src/core/Main.php
+++ src/core/Main.php
@@ -8,9 +8,14 @@ use pocketmine\Player;
 use pocketmine\plugin\Plugin;
 use pocketmine\utils\TextFormat as TF;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener{
    
-   public function onEnable()
-   {
+   public function onEnable(){
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+      $this->getLogger()->info(TF::GREEN . "ปลั๊กอิน §eCore" . TF::GREEN ." ถูกเปิดใช้งานแล้ว"); 
+      $this->Eco = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+   }else{
+      $this->getLogger()->info(TextFormat::RED . "ไม่พบปลั๊กอิน EconomyAPI");
+      $this->getPluginLoader()->disablePlugin($this);
    }
 }
