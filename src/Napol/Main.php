9d6c54d19e9a1ee0e203ed27f12629599738a964
NapolGamer TH <knightnapol15@gmail.com>
2018-03-12 22:43:47 +0700

    Update Main.php

--- src/Napol/Main.php
+++ src/Napol/Main.php
@@ -4,24 +4,26 @@ declare(strict_types=1);
 
 namespace Napol;
 
-use Napol\Command\FeedCommand;
-use Napol\Command\HealCommand;
-use Napol\Command\NickCommand;
-use Napol\Command\XYZCommand;
+use Napol\Command\Feed;
+use Napol\Command\Heal;
+use Napol\Command\Nick;
+use Napol\Command\XYZ;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat as C;
 class Main extends PluginBase {
    
+    const PERM_RANK = "§f- §cคุณไม่สามารถใช้คำสั่งนี้ได้!";
+    const PERM_STAFF = "§f- §cใช้ได้สำหรับแอดมินเท่านั้น!";
+    const USE_IN_GAME = "§f- §cใช้คำสั่งในเกมเท่านั้น!";
+   
     public function onEnable() {
-        $this->getServer()->getCommandMap()->register("feed", new FeedCommand($this));
-        $this->getServer()->getCommandMap()->register("heal", new HealCommand($this));
-        $this->getServer()->getCommandMap()->register("nick", new NickCommand($this));
-        $this->getServer()->getCommandMap()->register("gm", new GamemodeCommand($this));
-        $this->getServer()->getCommandMap()->register("xyz", new XYZCommand($this));
-        $this->getServer()->getPluginManager()->registerEvents(($this->joinmessage = new JoinMessage($this)), $this);
-        $this->getServer()->getPluginManager()->registerEvents(($this->jointitle = new JoinTitle($this)), $this);
+        $this->getServer()->getCommandMap()->register("fly", new Fly($this));
+        $this->getServer()->getCommandMap()->register("heal", new Heal($this));
+        $this->getServer()->getCommandMap()->register("nick", new Nick($this));
+        $this->getServer()->getCommandMap()->register("gm", new Gamemode($this));
+        $this->getServer()->getCommandMap()->register("xyz", new XYZ($this));
         $this->getLogger()->info("====================");
-        $this->getLogger()->info(C::WHITE."- ".C::GREEN."ปลั๊กอิน ".C::YELLOW."Core".C::GREEN." ถูกเปิดใช้งานแล้ว!");
+        $this->getLogger()->info("§f- §aปลั๊กอิน §eCore§a ถูกเปิดใช้งานแล้ว!");
         $this->getLogger()->info("====================");
     }
 }
